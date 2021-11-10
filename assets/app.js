9d7ea7f7e043eb1d56fa0137545eb26cac5c7c6c
mak-1920 <93209149+mak-1920@users.noreply.github.com>
2021-11-10 14:55:48 +0300

    before-auth

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,15 @@
-const $ = require('jquery');
-require('bootstrap');
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
 
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
+const $ = require('jquery');
+global.$ = global.jQuery = $;
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+// start the Stimulus application
+import './bootstrap';
--- config/bundles.php
+++ config/bundles.php
@@ -7,5 +7,7 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
