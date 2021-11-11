d7a39ee6f56d3fca2206985f39af01a16c29ad25
mak-1920 <93209149+mak-1920@users.noreply.github.com>
2021-11-11 03:51:42 +0300

    prod

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -36,16 +36,6 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('b75d866d96c2f69cedae@cloudmailin.net', 'Recommendations Mail Bot'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
-            // do anything else you need here, like send an email
-
             return $this->redirectToRoute('main');
         }
 
