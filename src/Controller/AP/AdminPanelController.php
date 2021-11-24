706fad17058c98e69ce4655cc6ad8320ab1ef430
mak-1920 <93209149+mak-1920@users.noreply.github.com>
2021-11-24 21:29:10 +0300

    ap

--- src/Controller/AP/AdminPanelController.php
+++ src/Controller/AP/AdminPanelController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller\AP;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Controller\BaseController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends AbstractController
+class AdminPanelController extends BaseController
 {
     #[Route('/{_locale<%app.locales%>}/ap', name: 'apanel')]
     public function index(): Response
     {
+        $users = $this->userRepository->findBy([], ['id' => 'ASC']);
+
         return $this->render('ap/index.html.twig', [
-            'controller_name' => 'MainController',
+            'users' => $users,
         ]);
     }
 }
